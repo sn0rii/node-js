48d3bc87ecb97a3fb46505c2ea3e10d62528470d
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-02-28 08:15:08 +0100

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,12 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperHeroName);
+console.log(mySuperVillainName);
+
+
+
+
